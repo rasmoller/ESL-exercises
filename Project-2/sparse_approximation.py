b2a583fe402691c767962ca0c0010ef74625aef8
rikke <rsuvsreesg@gmail.com>
2025-04-15 08:59:11 +0200

    main file

--- Project-2/sparse_approximation.py
+++ Project-2/sparse_approximation.py
@@ -4,13 +4,15 @@ import cr.sparse.pursuit.omp as omp
 import cr.sparse.pursuit.sp as sp
 import cr.sparse.cvx.adm.yall1 as bp
 
-import numpy as np
+import jax.numpy as jnp
+from jax import config
+config.update("jax_enable_x64", True)
 
 A_1 = AudioDictionaryLearner().learn_dictionary()
-A_2 = np.array()
+A_2 = jnp.array()
 
 class SparseApproximation():
-    def __init__(self, algorithm: callable, dictionary: np.array):
+    def __init__(self, algorithm: callable, dictionary: jnp.array):
         self.algorithm = algorithm
         self.dictionary = dictionary
 
